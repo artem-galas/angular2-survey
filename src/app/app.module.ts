bb2ac396a568c4d438d4b126ddc66f726b121e7f
artem_galas <artem.galas@gmail.com>
2016-11-04 17:53:09 +0200

    Create Home component Create RouteModule Styled Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material'
+import { RouterModule } from '@angular/router'
+import {routes} from './app.routes'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import {HomeRoutes} from './home/home.routes';
+
+export const routes: Routes = [
+  ...HomeRoutes
+];
