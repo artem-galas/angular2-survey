444deb995eba7640f87153d43ef27b40ca9b2289
artem-galas <artem.galas@gmail.com>
2016-11-06 16:10:53 +0200

    Create simple validation for Sign-up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material'
 import { RouterModule } from '@angular/router'
@@ -26,6 +26,7 @@ import { SignInComponent } from './sign-in/sign-in.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     MaterialModule.forRoot(),
     RouterModule.forRoot(routes)
   ],
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'survey-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
 
-  constructor() { }
+  public signUpForm: FormGroup;
+  public formSubmitted: boolean;
 
-  ngOnInit() {
+  constructor(private _fb:FormBuilder) {
+    this.signUpForm = _fb.group({
+      fullName: ['', Validators.required],
+      userName: ['', Validators.required],
+      email: ['', [
+        Validators.required,
+        Validators.pattern('.+?@.+?\\..+')]
+      ],
+      passwordGroup: _fb.group({
+        password: ['', [
+          Validators.required,
+          Validators.minLength(6)]
+        ],
+        passwordConfirmation: ['']
+      }, {validator: this.equalValidator })
+    });
+  }
+
+  public equalValidator({value}:FormGroup): {[key: string]: boolean} {
+    const [first,...rest] = Object.keys(value || {});
+    const valid = rest.every(v=>value[v] === value[first]);
+    return valid ? null : {equal: true}
+  }
+
+  public signUp(form) {
+    this.formSubmitted = true;
+    if (form.valid) {
+      //TODO Call service for create user
+      console.log(form.value);
+    }
   }
 
 }
