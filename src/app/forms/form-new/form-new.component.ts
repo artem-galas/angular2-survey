7050cc0593e8d40fcaabe2fbf8221ea9e73d1f23
artem-galas <artem.galas@gmail.com>
2016-11-12 22:21:12 +0200

    try to rebuild object to form

--- src/app/forms/form-new/form-new.component.ts
+++ src/app/forms/form-new/form-new.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Question } from "../../shared/classes/question";
 import { FieldComponent } from './field/field.component';
 import { Form } from '../../shared/classes/form';
+import {FormControl, FormGroup, FormBuilder, Validators, FormArray} from "@angular/forms"
 
 @Component({
   selector: 'survey-form-new',
@@ -31,9 +32,12 @@ export class FormNewComponent {
 
   public form:Form;
 
-  constructor() {
+  public formModule: FormGroup;
+
+  constructor(private _fb:FormBuilder) {
     this.form = new Form('');
     this.form.questions = this.questions;
+    this.buildFom();
   }
 
   public addQuestion() {
@@ -45,5 +49,67 @@ export class FormNewComponent {
     console.log(this.questions);
     console.log(this.form);
   }
+
+
+  // public buildFom() {
+  //   this.formModule = this._fb.group({
+  //     'name': [''],
+  //     'questions': new FormArray([
+  //       new FormControl()
+  //     ])
+  //   })
+  // }
+
+  public buildFom() {
+    this.formModule = this._fb.group({
+      'name': [''],
+      'questions': new FormArray([
+        new FormControl({
+          'text':''
+        })
+      ])
+    })
+  }
+
+  // public buildFom() {
+  //   this.formModule = this._fb.group({
+  //     'form': this._fb.group({
+  //       'name': [''],
+  //       'questions': new FormArray([
+  //         new FormControl({
+  //           'question': new FormControl({
+  //             'text': '',
+  //             'options': new FormArray([
+  //               new FormControl({
+  //                 'text': ''
+  //               })
+  //             ])
+  //           })
+  //         })
+  //       ])
+  //     })
+  //   })
+  // }
+
+
+  public saveForm1(form){
+    console.log(form.value);
+  }
+
+  public addQuestion1() {
+    console.log(this.formModule.get('questions'));
+    (this.formModule.get('questions') as FormArray)
+      .push(new FormControl(''));
+  }
+  //
+  //
+  // public addOption1() {
+  //   this.formModule['controls']('user')
+  //     .controls('questions')
+  //     .get('options')
+  //     .push(new FormControl({
+  //       'text': ''
+  //     }))
+  // }
 
 }
